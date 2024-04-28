dfeb5acb33add3d760227b39f44669a9aff5e28e
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-04-28 16:20:53 +0500

    working on search method

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:weather/weather.dart';
 import 'package:weatherapp/api.dart';
 
@@ -10,6 +11,7 @@ class Weatherscreen extends StatefulWidget {
 }
 
 class _WeatherscreenState extends State<Weatherscreen> {
+  TextEditingController weatherSearch = TextEditingController();
   //to get weather from weather package.      API
   final WeatherFactory wf = WeatherFactory(weatherAPI);
   //to store info for the weather coming from weather API.
@@ -30,7 +32,41 @@ class _WeatherscreenState extends State<Weatherscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey,
-      body: buildUI(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 2),
+          child: ListView(
+            children: [
+              Container(
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 20,
+                ),
+                width: MediaQuery.of(context).size.width,
+                height: 50,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white70,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextField(
+                  controller: weatherSearch,
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Search here...",
+                    prefixIcon: Icon(
+                      Icons.search,
+                      size: 30,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              buildUI(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
@@ -46,11 +82,104 @@ class _WeatherscreenState extends State<Weatherscreen> {
       height: MediaQuery.sizeOf(context).height,
       child: Column(
         mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          locationHeader(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.08,
+          ),
+          weatherIcon(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.05,
+          ),
+          currentTemp(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.02,
+          ),
+          dateTimeInfo(),
         ],
       ),
+    );
+  }
+
+  //function to show current area name.
+  Widget locationHeader() {
+    return Text(
+      weather?.areaName ?? "",
+      style: TextStyle(
+        fontSize: 30,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+//funtion to show current area's time.
+  Widget dateTimeInfo() {
+    return Column(
+      children: [
+        //this date format imported from intle package.
+        //Time.
+        Text(
+          DateFormat("h:mm a").format(
+            DateTime.now(),
+          ),
+          style: TextStyle(fontSize: 40),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        //Day.
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              DateFormat("EEEE").format(
+                DateTime.now(),
+              ),
+              style: TextStyle(fontSize: 25),
+            ),
+            //Date.
+            Text(
+              "  ${DateFormat("d.m.y").format(
+                DateTime.now(),
+              )}",
+              style: TextStyle(fontSize: 25),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  //function to show weather Icon.
+  Widget weatherIcon() {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.sizeOf(context).height * 0.20,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  "http://openweathermap.org/img/wn/${weather?.weatherIcon}@4x.png"),
+            ),
+          ),
+        ),
+        Text(weather?.weatherDescription ?? ""),
+      ],
+    );
+  }
+
+  //function to show current temperature.
+  Widget currentTemp() {
+    return Text(
+      "${weather?.temperature?.celsius?.toStringAsFixed(0)}\u00B0C",
+      style: TextStyle(),
     );
   }
 }
